cb66f90b0fe97c33c31f33c972be1317a3638508
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-12 15:51:39 -0400

    First reek pass.

--- lib/mystiko/generator.rb
+++ lib/mystiko/generator.rb
@@ -6,8 +6,12 @@ require 'composite_rng'
 #The internal pseudo-random number generator.
 class Mystiko
 
+  #A specialized variant of the composite pseudo RNG.
   class Generator < CompositeRng
 
+    #Create the default pseudo random data generator.
+    #<br> Endemic Code Smells
+    #* :reek:FeatureEnvy
     def initialize(key)
       parent = FibonacciRng.new(key)
       child = Random.new(parent.hash_value)
@@ -16,6 +20,4 @@ class Mystiko
 
   end
 
-
 end
-
