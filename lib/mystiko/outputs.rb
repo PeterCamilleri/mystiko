b0f1f5dff759b6c2bc012985bea49f85d9b0cc29
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-13 11:09:58 -0400

    Added output arg processing.

--- lib/mystiko/outputs.rb
+++ lib/mystiko/outputs.rb
@@ -7,8 +7,14 @@ class Mystiko
   def process_outputs(args)
     @input, @generator, @window, @data = nil, nil, nil, nil
 
-  end
+    if (name = args[:out_file])
+      IO.write(name, @output)
+    elsif (out_str = args[:out_str])
+      out_str << @output
+    end
 
+    (_, @output = @output, nil)[0]
+  end
 
 end
 
