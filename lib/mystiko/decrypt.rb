e2058879f8695fa8caadb5a2ac31e3510e901482
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-14 08:02:32 -0400

    Set up the decrypt skeleton.

--- lib/mystiko/decrypt.rb
+++ lib/mystiko/decrypt.rb
@@ -6,8 +6,25 @@ class Mystiko
   #Perform data decryption
   def decrypt(args={})
     process_inputs(args)
-
+    setup_cypher_input
+    do_decryption
+    setup_clear_output
     process_outputs(args)
+  end
+
+  #Get the cypher (aka encrypted) input data set up.
+  def setup_cypher_input
+
+  end
+
+  #Do the actual decryption work.
+  def do_decryption
+
+  end
+
+  #Get the clear (aka unencrypted) output data set up.
+  def setup_clear_output
+
   end
 
 end
