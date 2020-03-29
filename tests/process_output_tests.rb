02b970a1418495eb8d9c8d66bd9731314ef68280
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-03-29 14:39:07 -0400

    Updated tests to use assert_nil

--- tests/process_output_tests.rb
+++ tests/process_output_tests.rb
@@ -17,21 +17,21 @@ class ProcessOutputsTester < Minitest::Test
     mystiko.output = "ABC"
     args = {  }
     assert_equal("ABC", mystiko.process_outputs(args))
-    assert_equal(nil, mystiko.output)
+    assert_nil(mystiko.output)
 
     mystiko.output = "ABC"
     str = ""
     args = { out_str: str  }
     assert_equal("ABC", mystiko.process_outputs(args))
     assert_equal("ABC", str)
-    assert_equal(nil, mystiko.output)
+    assert_nil(mystiko.output)
 
     mystiko.output = "ABC"
     out_name = "tests/output.txt"
     args = { out_file: out_name }
     assert_equal("ABC", mystiko.process_outputs(args))
     assert(File.exist?(out_name))
-    assert_equal(nil, mystiko.output)
+    assert_nil(mystiko.output)
     assert_equal("ABC", IO.read(out_name))
     system("rm #{out_name}")
     refute(File.exist?(out_name))
@@ -43,10 +43,10 @@ class ProcessOutputsTester < Minitest::Test
     mystiko.output = "ABC"
     args = { in_file: "tests/input.txt", key: "fubar", window: 24 }
     assert_equal("ABC", mystiko.process_outputs(args))
-    assert_equal(nil, mystiko.output)
-    assert_equal(nil, mystiko.input)
-    assert_equal(nil, mystiko.window)
-    assert_equal(nil, mystiko.generator)
+    assert_nil(mystiko.output)
+    assert_nil(mystiko.input)
+    assert_nil(mystiko.window)
+    assert_nil(mystiko.generator)
   end
 
 end
